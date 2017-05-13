efad35ef6759af2e1988b6594c8d63b33ba4de02
Yasuaki Uechi <uetchy@randompaper.co>
2017-05-13 09:07:34 +0900

    code style

--- test/gitconfig.spec.js
+++ test/gitconfig.spec.js
@@ -1,79 +1,85 @@
-import {homedir} from 'os';
-import fs from 'fs';
-import test from 'ava';
-import mockery from 'mockery';
+import { homedir } from 'os'
+import fs from 'fs'
+import {join} from 'path'
+import test from 'ava'
+import mockery from 'mockery'
 
-const gitconfigPath = '../lib/gitconfig';
+const gitconfigPath = '../lib/gitconfig'
 
-const GLOBAL_CONFIG_SOURCE = fs.readFileSync('./fixture/global_config', 'utf-8');
+const GLOBAL_CONFIG_SOURCE = fs.readFileSync(join(__dirname, 'fixture/global_config'), 'utf-8')
 const GLOBAL_CONFIG_EXPECTED = {
-	user: {
-		name: 'Globe Trotter',
-		email: 'globe.trotter@example.com'
-	}
-};
-const LOCAL_CONFIG_SOURCE = fs.readFileSync('./fixture/local_config', 'utf-8');
-const LOCAL_CONFIG_EXPECTED = { // eslint-disable-line quote-props
-	user: {
-		name: 'Local Officer', email: 'local.officer@example.com'
-	},
-	'remote "origin"': {
-		url: 'ssh://git@github.com/uetchy/git-account',
-		fetch: '+refs/heads/*:refs/remotes/origin/*',
-		gtPrivateKeyPath: '~/.ssh/id_rsa_local_officer'
-	},
-	'branch "master"': {
-		remote: 'origin',
-		merge: 'refs/heads/master'
-	}
-};
+  user: {
+    name: 'Globe Trotter',
+    email: 'globe.trotter@example.com'
+  }
+}
+const LOCAL_CONFIG_SOURCE = fs.readFileSync(join(__dirname, 'fixture/local_config'), 'utf-8')
+const LOCAL_CONFIG_EXPECTED = {
+  // eslint-disable-line quote-props
+  user: {
+    name: 'Local Officer',
+    email: 'local.officer@example.com'
+  },
+  'remote "origin"': {
+    url: 'ssh://git@github.com/uetchy/git-account',
+    fetch: '+refs/heads/*:refs/remotes/origin/*',
+    gtPrivateKeyPath: '~/.ssh/id_rsa_local_officer'
+  },
+  'branch "master"': {
+    remote: 'origin',
+    merge: 'refs/heads/master'
+  }
+}
 
 test.serial('get global gitconfig', async t => {
-	const fsMock = {
-		readFileSync: path => {
-			t.is(path, `${homedir()}/.gitconfig`);
-			return GLOBAL_CONFIG_SOURCE;
-		}
-	};
+  const fsMock = {
+    readFile: (path) => {
+      return new Promise((resolve, reject) => {
+        console.log("red")
+        t.is(path, `${homedir()}/.gitconfig`)
+        return resolve(GLOBAL_CONFIG_SOURCE)
+      })
+    }
+  }
 
-	mockery.registerAllowable(gitconfigPath);
-	mockery.registerMock('fs', fsMock);
-	mockery.enable({
-		useCleanCache: true,
-		warnOnReplace: false,
-		warnOnUnregistered: false
-	});
+  mockery.registerAllowable(gitconfigPath)
+  mockery.registerMock('fs', fsMock)
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
 
-	const gitconfig = require(gitconfigPath);
-	const config = await gitconfig.getGlobalConfig();
+  const gitconfig = require(gitconfigPath)
+  const config = await gitconfig.getGlobalConfig()
 
-	t.deepEqual(config, GLOBAL_CONFIG_EXPECTED);
+  t.deepEqual(config, GLOBAL_CONFIG_EXPECTED)
 
-	mockery.disable();
-	mockery.deregisterAll();
-});
+  mockery.disable()
+  mockery.deregisterAll()
+})
 
 test.serial('get local gitconfig', async t => {
-	const fsMock = {
-		readFileSync: path => {
-			t.is(path, `${process.cwd()}/.git/config`);
-			return LOCAL_CONFIG_SOURCE;
-		}
-	};
+  const fsMock = {
+    readFileSync: path => {
+      t.is(path, `${process.cwd()}/.git/config`)
+      return LOCAL_CONFIG_SOURCE
+    }
+  }
 
-	mockery.registerAllowable(gitconfigPath);
-	mockery.registerMock('fs', fsMock);
-	mockery.enable({
-		useCleanCache: true,
-		warnOnReplace: false,
-		warnOnUnregistered: false
-	});
+  mockery.registerAllowable(gitconfigPath)
+  mockery.registerMock('fs', fsMock)
+  mockery.enable({
+    useCleanCache: true,
+    warnOnReplace: false,
+    warnOnUnregistered: false
+  })
 
-	const gitconfig = require(gitconfigPath);
-	const config = await gitconfig.getLocalConfig();
+  const gitconfig = require(gitconfigPath)
+  const config = await gitconfig.getLocalConfig()
 
-	t.deepEqual(config, LOCAL_CONFIG_EXPECTED);
+  t.deepEqual(config, LOCAL_CONFIG_EXPECTED)
 
-	mockery.disable();
-	mockery.deregisterAll();
-});
+  mockery.disable()
+  mockery.deregisterAll()
+})
